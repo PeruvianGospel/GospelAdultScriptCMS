149e5e5ea27ccca7999d00cd1d20c55fe815b6b7
PeruvianGospel <theperuviangospel@gmail.com>
2023-07-20 19:46:38 -0500

    Primer comit antes de instalar ADMIN LTE

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="grid grid-cols-4">
+    <div class="bg-blue-500 text-white">A</div>
+    <div class="bg-red-500">B</div>
+    <div class="bg-blue-500">C</div>
+    <div class="bg-blue-500">D</div>
+  </div>
+</body>
+</html>
