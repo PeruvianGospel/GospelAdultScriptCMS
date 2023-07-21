d43a117dc4f2e6c3ade5fe1b826e56426233eda5
PeruvianGospel <theperuviangospel@gmail.com>
2023-07-20 19:53:12 -0500

    segundo comit

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/VIDEO',function(){
+    return "hola";
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
